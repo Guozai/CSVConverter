da1562ae01a823be97cb969548e9fba37f6820b9
Guozai <yiping.guo@gmail.com>
2018-03-21 20:59:17 +1100

    fixed for loop error so the second last item is written into the output file.

--- src/CSVConverter.java
+++ src/CSVConverter.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
-        import java.io.FileReader;
-        import java.io.IOException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 public class CSVConverter {
     public static void main(String[] args) {
@@ -12,16 +13,27 @@ public class CSVConverter {
 
         int count = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            while ((eachline = br.readLine()) != null) {
-                String[] splited = eachline.split(cvsSpliter);
-                String s = "";
-                for (int i = 0; i < splited.length - 2; i++) {
-                    s = splited[i];
-                    System.out.print(s + ",");
+            try (PrintWriter out = new PrintWriter("output.csv")) {
+                while ((eachline = br.readLine()) != null) {
+                    // split each tab-separated line into token array
+                    String[] splited = eachline.split(cvsSpliter);
+                    // temporary variable to store tokens
+                    String s = "";
+                    for (int i = 0; i < splited.length - 1; i++) {
+                        s = splited[i];
+                        // write to fire changing tab to comma
+                        out.print(s + ",");
+                        System.out.print(s + ",");
+                    }
+                    s = splited[splited.length - 1];
+                    // the end of each line does not need comma
+                    out.println(s);
+                    System.out.println(s);
+                    // count number of lines
+                    count++;
                 }
-                s = splited[splited.length - 1];
-                System.out.println(s);
-                count++;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
             System.out.println(count);
         } catch (IOException e){
