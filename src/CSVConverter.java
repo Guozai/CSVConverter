8f8c87afa886d7e25e2d7bbf459a3e7ad5440ce7
Guozai <yiping.guo@gmail.com>
2018-03-21 19:12:55 +1100

    tokenize each line

--- src/CSVConverter.java
+++ src/CSVConverter.java
@@ -1,17 +1,21 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+        import java.io.FileReader;
+        import java.io.IOException;
 
 public class CSVConverter {
     public static void main(String[] args) {
         String csvFile = "BUSINESS_NAMES_201803.csv";
-//        if (args[0] != null)
-//            csvFile = args[0];
+        if (args[0] != null)
+            csvFile = args[0];
         String eachline = "";
+        String cvsSpliter = "\\s+";
 
         int count = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while ((eachline = br.readLine()) != null) {
+                String[] splited = eachline.split(cvsSpliter);
+                for (String s : splited)
+                    System.out.print(s + ", ");
                 count++;
             }
             System.out.println(count);
