5a70faf3bd8b8de4eea8e6e7b344b3750a803633
Guozai <yiping.guo@gmail.com>
2018-03-21 18:56:49 +1100

    Can count the number of lines of the source .csv file

--- src/CSVConverter.java
+++ src/CSVConverter.java
@@ -3,5 +3,20 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CSVConverter {
+    public static void main(String[] args) {
+        String csvFile = "BUSINESS_NAMES_201803.csv";
+//        if (args[0] != null)
+//            csvFile = args[0];
+        String eachline = "";
 
+        int count = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((eachline = br.readLine()) != null) {
+                count++;
+            }
+            System.out.println(count);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
