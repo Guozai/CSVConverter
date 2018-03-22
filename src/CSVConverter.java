40623e98be53092c2eec83283142794d6c6d4c82
Guozai <yiping.guo@gmail.com>
2018-03-23 00:30:27 +1100

    Done

--- src/CSVConverter.java
+++ src/CSVConverter.java
@@ -6,34 +6,18 @@ import java.io.IOException;
 public class CSVConverter {
     //private static String csvFile = "BUSINESS_NAMES_201803.csv";
     private static String csvFile = "asic.csv";
+    private static String registerName = "BUSINESS NAMES";
+    private static String[] status = new String[] {"Registered", "Deregistered"};
+    private static String[] states = new String[] {"ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC", "WA"};
     private static String eachline = "";
+    private static String cvsSpliter = "\\t";
 
     private static void CSVtoCSV() {
-        String cvsSpliter = "\\t";
-        String[] registerNames;
-
         // line counter
         int count = 0;
         // convert the source file to file with comma splitter
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            try (PrintWriter out = new PrintWriter("output.csv")) {
-                // the first line (header line) will not be changed
-                if ((eachline = br.readLine()) != null) {
-                    // count number of lines
-                    count++;
-                    // split each tab-separated line into token array
-                    String[] splited = eachline.split(cvsSpliter);
-                    // temporary variable to store tokens
-                    String s = "";
-                    for (int i = 0; i < splited.length - 1; i++) {
-                        s = splited[i];
-                        // write to fire changing tab to comma
-                        out.print(s + ",");
-                    }
-                    s = splited[splited.length - 1];
-                    // the end of each line does not need comma
-                    out.println(s);
-                }
+            try (PrintWriter out = new PrintWriter("bn.csv")) {
                 while ((eachline = br.readLine()) != null) {
                     // count number of lines
                     count++;
@@ -41,14 +25,81 @@ public class CSVConverter {
                     String[] splited = eachline.split(cvsSpliter);
                     // temporary variable to store tokens
                     String s = "";
-                    for (int i = 0; i < splited.length - 1; i++) {
+                    for (int i = 0; i < splited.length; i++) {
                         s = splited[i];
-                        // write to fire changing tab to comma
-                        out.print(s + ",");
+                        // the first line (header line) will not be changed
+                        if (count == 1) {
+                            // write to fire changing tab to comma
+                            out.print(s);
+                            // put comma behind each token except the last one
+                            if (i < splited.length - 1)
+                                out.print(",");
+                            else
+                                // end of line
+                                out.println();
+                        } else {
+                            int id;
+                            switch (s) {
+                                // column REGISTER_NAME type 1
+                                case "BUSINESS NAMES":
+                                    id = 1;
+                                    out.print(id);
+                                    break;
+                                // column BN_STATUS
+                                case "Registered":
+                                    id = 1;
+                                    out.print(id);
+                                    break;
+                                case "Deregistered":
+                                    id = 2;
+                                    out.print(id);
+                                    break;
+                                // column BN_STATE_OF_REG
+                                case "NSW":
+                                    id = 2;
+                                    out.print(id);
+                                    break;
+                                case "VIC":
+                                    id = 7;
+                                    out.print(id);
+                                    break;
+                                case "QLD":
+                                    id = 4;
+                                    out.print(id);
+                                    break;
+                                case "WA":
+                                    id = 8;
+                                    out.print(id);
+                                    break;
+                                case "SA":
+                                    id = 5;
+                                    out.print(id);
+                                    break;
+                                case "ACT":
+                                    id = 1;
+                                    out.print(id);
+                                    break;
+                                case "TAS":
+                                    id = 6;
+                                    out.print(id);
+                                    break;
+                                case "NT":
+                                    id = 3;
+                                    out.print(id);
+                                    break;
+                                default:
+                                    // write to fire changing tab to comma
+                                    out.print(s);
+                                    break;
+                            }
+                            // put comma behind each token except the last one
+                            if (i < splited.length - 1)
+                                out.print(",");
+                            else
+                                // end of line
+                                out.println();
+                        }
                     }
-                    s = splited[splited.length - 1];
-                    // the end of each line does not need comma
-                    out.println(s);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -59,10 +110,32 @@ public class CSVConverter {
         }
     }
 
+    private static void createCSV(String[] dataset, String fileName) {
+        try (PrintWriter out = new PrintWriter(fileName)) {
+            for (int i = 0; i < dataset.length; i++) {
+                out.print((i+1) + ",");
+                out.println(dataset[i]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void createCSVSingleString(String data, String fileName) {
+        try (PrintWriter out = new PrintWriter(fileName)) {
+                out.println(1 + "," + data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis(); // timer for calculating program execution time
 
         CSVtoCSV();
+        createCSVSingleString(registerName, "regName.csv");
+        createCSV(status, "status.csv");
+        createCSV(states, "states.csv");
 
 
         long stopTime = System.currentTimeMillis();
