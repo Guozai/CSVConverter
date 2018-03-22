7243901c42a031c7f590be25709f35f1c38f1b38
Guozai <yiping.guo@gmail.com>
2018-03-22 22:13:20 +1100

    split CSVConverter into methods

--- src/CSVConverter.java
+++ src/CSVConverter.java
@@ -4,18 +4,36 @@ import java.io.PrintWriter;
 import java.io.IOException;
 
 public class CSVConverter {
-    public static void main(String[] args) {
-        long startTime = System.currentTimeMillis(); // timer for calculating program execution time
-        String csvFile = "BUSINESS_NAMES_201803.csv";
-        if (args[0] != null)
-            csvFile = args[0];
-        String eachline = "";
-        String cvsSpliter = "\\t";
-        //String[] registerNames;
+    //private static String csvFile = "BUSINESS_NAMES_201803.csv";
+    private static String csvFile = "asic.csv";
+    private static String eachline = "";
 
+    private static void CSVtoCSV() {
+        String cvsSpliter = "\\t";
+        String[] registerNames;
+
+        // line counter
         int count = 0;
+        // convert the source file to file with comma splitter
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             try (PrintWriter out = new PrintWriter("output.csv")) {
+                // the first line (header line) will not be changed
+                if ((eachline = br.readLine()) != null) {
+                    // count number of lines
+                    count++;
+                    // split each tab-separated line into token array
+                    String[] splited = eachline.split(cvsSpliter);
+                    // temporary variable to store tokens
+                    String s = "";
+                    for (int i = 0; i < splited.length - 1; i++) {
+                        s = splited[i];
+                        // write to fire changing tab to comma
+                        out.print(s + ",");
+                    }
+                    s = splited[splited.length - 1];
+                    // the end of each line does not need comma
+                    out.println(s);
+                }
                 while ((eachline = br.readLine()) != null) {
                     // count number of lines
                     count++;
@@ -27,12 +45,10 @@ public class CSVConverter {
                         s = splited[i];
                         // write to fire changing tab to comma
                         out.print(s + ",");
-                        //System.out.print(s + ",");
                     }
                     s = splited[splited.length - 1];
                     // the end of each line does not need comma
                     out.println(s);
-                    //System.out.println(s);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -41,68 +57,16 @@ public class CSVConverter {
         } catch (IOException e){
             e.printStackTrace();
         }
+    }
 
-//        // convert the source file to file with comma splitter
-//        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-//            try (PrintWriter out = new PrintWriter("output.csv")) {
-//                while ((eachline = br.readLine()) != null) {
-//                    // split each tab-separated line into token array
-//                    String[] splited = eachline.split(cvsSpliter);
-//                    // temporary variable to store tokens
-//                    String s = "";
-//                    for (int i = 0; i < splited.length - 1; i++) {
-//                        s = splited[i];
-//                        // write to fire changing tab to comma
-//                        out.print(s + ",");
-//                        //System.out.print(s + ",");
-//                    }
-//                    s = splited[splited.length - 1];
-//                    // the end of each line does not need comma
-//                    out.println(s);
-//                    //System.out.println(s);
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        } catch (IOException e){
-//            e.printStackTrace();
-//        }
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis(); // timer for calculating program execution time
 
-//        int count = 0;
-//        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-//            try (PrintWriter out = new PrintWriter("output.csv")) {
-//                while ((eachline = br.readLine()) != null) {
-//                    // count number of lines
-//                    count++;
-//                    // split each tab-separated line into token array
-//                    String[] splited = eachline.split(cvsSpliter);
-//                    // temporary variable to store tokens
-//                    String s = "";
-//                    for (int i = 0; i < splited.length - 1; i++) {
-//                        s = splited[i];
-//                        // the first line (header line) will not be changed
-//                        if (count == 1) {
-//                            // write to fire changing tab to comma
-//                            out.print(s + ",");
-//                            //System.out.print(s + ",");
-//                        }
-//                    }
-//                    s = splited[splited.length - 1];
-//                    // the end of each line does not need comma
-//                    out.println(s);
-//                    //System.out.println(s);
-//
-//                }
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println(count);
-//        } catch (IOException e){
-//            e.printStackTrace();
-//        }
+        CSVtoCSV();
+
 
         long stopTime = System.currentTimeMillis();
-        double elapsedTime = (stopTime - startTime) / 1000.0;
+        double elapsedTime = (stopTime - startTime) / 1000.0; // execution time of the program in seconds
         System.out.println("execution time:          " + elapsedTime + " s");
     }
 }
--- src/MongoJDBC.java
+++ src/MongoJDBC.java
@@ -1,19 +1,39 @@
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
+//import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 
 public class MongoJDBC {
     public static void main(String args[]) {
         try {
             // connect to MongoDB
-            MongoClient mongodb  = new MongoClient("localhost", 27017);
+            MongoClient mongodb  = new MongoClient("ec2-user@s3177105.cosc2406.route53.aws.rmit.edu.au", 27017);
 
             // get database
-            DB db = mongodb.getDB("assignment1");
+            MongoDatabase db = mongodb.getDatabase("assignment1");
             System.out.println("Connect to database successfully");
 
             // get collection from database
-            DBCollection table = db.getCollection("user");
+            MongoCollection contacts = db.getCollection("contacts");
+
+            // insert
+            Document document = new Document("name", "mkyong");
+//            document.put("age", 30);
+//            document.put("createdDate", new Date());
+            contacts.insertOne(document);
+
+            FindIterable findIterable = contacts.find();
+            Iterator iterator = findIterable.iterator();
+            while(iterator.hasNext()) {
+                Document contact = (Document) iterator.next();
+                List<Document> contactNames = (List<Document>) ((Document) contact.get("metadata")).get("name");
+                System.out.println(contactNames);
+            }
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
