c5514e7bfb5be97f06002f08f1ff2833833e7a2d
Guozai <yiping.guo@gmail.com>
2018-03-23 01:42:34 +1100

    JDBC working - inserted data into collection

--- src/MongoJDBC.java
+++ src/MongoJDBC.java
@@ -1,9 +1,8 @@
-//import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
+//import java.util.Iterator;
+//import java.util.List;
 
 import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
+//import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
@@ -12,28 +11,27 @@ public class MongoJDBC {
     public static void main(String args[]) {
         try {
             // connect to MongoDB
-            MongoClient mongodb  = new MongoClient("ec2-user@s3177105.cosc2406.route53.aws.rmit.edu.au", 27017);
+            MongoClient mongodb  = new MongoClient("localhost", 27017);
 
             // get database
             MongoDatabase db = mongodb.getDatabase("assignment1");
             System.out.println("Connect to database successfully");
 
             // get collection from database
-            MongoCollection contacts = db.getCollection("contacts");
+            MongoCollection stateOfRegister = db.getCollection("stateOfRegister");
 
             // insert
-            Document document = new Document("name", "mkyong");
-//            document.put("age", 30);
-//            document.put("createdDate", new Date());
-            contacts.insertOne(document);
+            Document document = new Document("state_id", 9);
+            document.put("BN_STATE_OF_REG", "AAA");
+            stateOfRegister.insertOne(document);
 
-            FindIterable findIterable = contacts.find();
-            Iterator iterator = findIterable.iterator();
-            while(iterator.hasNext()) {
-                Document contact = (Document) iterator.next();
-                List<Document> contactNames = (List<Document>) ((Document) contact.get("metadata")).get("name");
-                System.out.println(contactNames);
-            }
+//            FindIterable findIterable = stateOfRegister.find();
+//            Iterator iterator = findIterable.iterator();
+//            while(iterator.hasNext()) {
+//                Document state = (Document) iterator.next();
+//                List<Document> states = (List<Document>) ((Document) state.get("metadata")).get("BN_STATE_OF_REG");
+//                System.out.println(states);
+//            }
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
