cacbe5ee93ea7a53612671fcccbbd5c70a8a170a
Guozai <yiping.guo@gmail.com>
2018-03-24 23:56:37 +1100

    dbload arguments working

--- src/dbload.java
+++ src/dbload.java
@@ -1,10 +1,22 @@
 public class dbload {
-    private static int pageSize = 512;
+    private static int pageSize = 4096;
     private static String fileName = "asic.csv";
     public static void main(String[] args) {
-//        if (args[0] == "-p") {
-//            pageSize = Integer.parseInt(args[1]);
-//            fileName = args[2];
+//        try {
+//            if (args.length == 3) {
+//                if (args[0].equals("-p")) {
+//                    pageSize = Integer.parseInt(args[1]);
+//                    fileName = args[2];
+//                } else {
+//                    throw new Exception("First argument is not \"-p\"");
+//                }
+//            } else {
+//                throw new Exception("Need 3 arguments: java dbload -p pagesize datafile");
+//            }
+//        } catch (NumberFormatException e) {
+//            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+//        } catch (Exception e) {
+//            System.err.println(e.getClass().getName() + ": " + e.getMessage());
 //        }
 
         HeapFileCreater heapFileCreater = new HeapFileCreater();
