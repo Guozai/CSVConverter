23b2e7af0cca19e9a780c50c7edc1e341f4f9bd2
Guozai <yiping.guo@gmail.com>
2018-03-25 14:53:20 +1100

    done reading the length of the first item in binary file

--- src/HeapSearch.java
+++ src/HeapSearch.java
@@ -1,6 +1,11 @@
+import java.io.*;
+
 public class HeapSearch {
     private String queryKey;
     private int pageSize;
+
+    private int pos = 0; // position pointer
+    private byte[] buf; // byte[] buffer
 
     public HeapSearch (String queryKey, int pageSize) {
         this.queryKey = queryKey;
@@ -8,6 +13,20 @@ public class HeapSearch {
     }
 
     public void launch() {
+        // read the source file
+        try (FileInputStream fileIn = new FileInputStream(new File("heap." + Integer.toString(pageSize)))) {
+            DataInputStream in = new DataInputStream(fileIn);
+            int lenStr = (int) in.readByte();
+//            System.out.println((int) in.readByte());
+            pos += 1;
+//            //in.read(buf, pos, 1);
+            in.read(buf, pos, lenStr);
 
+            fileIn.close();
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
     }
 }
